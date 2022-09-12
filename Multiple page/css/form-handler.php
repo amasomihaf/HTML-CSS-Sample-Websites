31f473a45ab09cc6a371df725e0ba46970025325
Muhammad Osama Faheem <112871022+amasomihaf@users.noreply.github.com>
2022-09-12 16:06:16 +0500

    Add files via upload

---
+++ Multiple page/css/form-handler.php
@@ -0,0 +1,23 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_form = 'info@multiuniversitypage.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name : $name.\n".
+               "User Email: $visitor_email.\n".
+                "Subject: $subject.\n".
+                "User Message: $message .\n ";
+
+$to = 'osifahim1996@gmail.com';
+$headers = "Form: $email_form \r\n";
+$headers .= "Replay-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+?>
